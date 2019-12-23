c07a8b8934ddae2796b13dda9fdb2c76b5e597c2
Tomy MANSON <mansoninformatique@gmail.com>
2019-12-23 22:52:15 +0100

    Ajout authentification par token

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class UserFixtures extends Fixture implements FixtureGroupInterface, OrderedFixtureInterface, ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * @inheritDoc
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $encoder = $this->container->get('security.password_encoder');
+        $user = new User();
+        $user->setApiToken('test_api_token');
+        $user->setUsername('test');
+        $user->setPassword($encoder->encodePassword(
+            $user,
+            'test'
+        ));
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getGroups(): array
+    {
+        return ['test', 'test-materials'];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getOrder()
+    {
+        return 10;
+    }
+
+
+}
